512ddd0254032d64ed11ce2ac878b93207290e45
Colton <colton.glasgow@outlook.com>
2017-03-10 16:12:40 -0800

    Add files via upload

---
+++ startup.rb
@@ -0,0 +1,4 @@
+system('echo "/apps/DarkLight/DarkLight" >> /etc/rc.d/rc.local')
+system("wget -O /root/Desktop/darklight.lic http://www.champtc.com/installer/da$
+system('sh /etc/dl/DarkLight*')
+system('/apps/DarkLight/DarkLight')
