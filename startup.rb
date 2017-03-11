8199a074662b1d47a4b3b718a4cb4bf2d46e2f38
Colton <colton.glasgow@outlook.com>
2017-03-10 16:27:23 -0800

    Update startup.rb

--- startup.rb
+++ startup.rb
@@ -1,4 +1,4 @@
 system('echo "/apps/DarkLight/DarkLight" >> /etc/rc.d/rc.local')
-system("wget -O /root/Desktop/darklight.lic http://www.champtc.com/installer/da$
+system("wget -O /root/Desktop/darklight.lic http://www.champtc.com/installer/darklight-trial.lic")
 system('sh /etc/dl/DarkLight*')
 system('/apps/DarkLight/DarkLight')
